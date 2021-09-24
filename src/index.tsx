0cc8d93dcf47536bdc81acfe6bbfbd8a49a1fc56
hyungsangpark <0916bacon@gmail.com>
2021-09-24 15:41:16 +1200

    Added Web3 and added minimum required codes to interact with web3.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-    const account = await web3.eth.getAccounts();
-    // console.log(account);
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
+import { provider } from "web3-core";
+
+function getLibrary(provider: provider) {
+  const web3 = new Web3(provider);
+  return new Web3(provider);
+}
 
 ReactDOM.render(
   <Web3ReactProvider getLibrary={getLibrary}>

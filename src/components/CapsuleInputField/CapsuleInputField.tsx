f08704432d6ff0d87cba927f01a371a957efc127
hyungsangpark <0916bacon@gmail.com>
2021-09-27 16:36:31 +1300

    Fixed Input field to adapt to various use cases.

--- src/components/CapsuleInputField/CapsuleInputField.tsx
+++ src/components/CapsuleInputField/CapsuleInputField.tsx
@@ -1,28 +1,42 @@
-import React, { ChangeEventHandler } from "react";
+import React, { ChangeEventHandler, FormEventHandler } from "react";
 
 import "./CapsuleInputField.css";
 
 interface ICapsuleInputFieldProps {
+  rows?: number;
   value?: string;
   forCurrency?: boolean;
-  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
+  className?: string;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
 }
 
 function CapsuleInputField({
   value,
-  onChange,
   forCurrency,
+  rows,
+  className,
+  onChange,
 }: ICapsuleInputFieldProps) {
   const setUnit = (value: string | undefined) => {
     value = value ?? "0 ETH";
+    if (value.endsWith(" ET")) value = value.substring(0, value.length - 4);
     return value.replace(new RegExp("([ ETH]+)"), "") + " ETH";
   };
 
-  return (
-    <textarea
-      className="capsule-input-field"
-      // rows={1}
+  if (rows === undefined || rows < 2) rows = 1;
+
+  return (rows === 1) ? (
+    <input
+      className={`capsule-input-field ${className}`}
+      type="text"
       value={forCurrency ? setUnit(value) : value}
+      onChange={onChange}
+    />
+  ) : (
+    <textarea
+      className={`capsule-input-field ${className}`}
+      rows={rows}
+      value={value}
       onChange={onChange}
     />
   );
